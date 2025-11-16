d70035acacd10290eac52a2e3bfd8cf1542a23ab
Tomasz Kopacz <tkopacz@users.noreply.github.com>
2025-11-16 16:12:24 +0100

    commit

--- SecurityDemo/PathTraversalVulnerabilities.cs
+++ SecurityDemo/PathTraversalVulnerabilities.cs
@@ -46,6 +46,7 @@ public class PathTraversalVulnerabilities
             return File.ReadAllBytes(fullPath);
         }
         return Array.Empty<byte>();
+        //DODANY KOMENTARZ
     }
 
     /// <summary>
